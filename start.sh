482237e6ec59b3319237d8a1718ffc6261b92b0f
pitastrudl <pitastrudl@gmail.com>
2022-08-05 20:44:00 +0200

    Added start helper script

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ansible-playbook site.yml -i inventory.yml 
+skip_tags=""
+if [ -z "$skip_tags" ]; then
+   skip_tags="--skip-tags $1"
+fi
+
+ANSIBLE_CONFIG=ansible.cfg   ansible-playbook -v site.yml -i inventory.yml  $skip_tags -e 'ansible_python_interpreter=/usr/bin/python3'
+
+echo "$skip_tags"
